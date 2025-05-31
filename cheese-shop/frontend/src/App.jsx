5997928da2da4c7c360d2c6fd68e96c46c5d3c93
AntomioGL <a.gastelum23@info.uas.edu.mx>
2025-05-30 17:12:17 -0700

    Add files via upload

---
+++ cheese-shop/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import CheeseForm from "./components/CheeseForm";
+import CheeseList from "./components/CheeseList";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Tienda de Quesos</h1>
+      <CheeseForm />
+      <CheeseList />
+    </div>
+  );
+}
+
+export default App;
